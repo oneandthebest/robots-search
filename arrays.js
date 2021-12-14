671e0e06142f8c7f99699cded868a4f09e7839e8
leopantsulaia <levanpantsulaia@yahoo.com>
2021-12-15 03:40:58 +0400

    adding files for github pages

---
+++ arrays.js
@@ -0,0 +1,55 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-restricted-globals */
+/* eslint-disable no-undef */
+
+// myArray COMMENT
+// const myArray = [1,2,3,4];
+
+// map();
+
+// filter();
+
+// reduce();
+
+// find();
+
+// includes();
+// myArray COMMENT
+
+// myPromise COMMENT
+// const myPromise = new Promise((resolve, reject) => {
+//   if (true) {
+//     setTimeout(() => {
+//       resolve('I have succeeded');
+//     }, 1000);
+//   }
+// });
+
+// myPromise
+//   .then((value) => value + '!!!!!')
+//   .then((newValue) => console.log(newValue))
+//   .catch((rejectValue) => console.log(rejectValue));
+// myPromise COMMENT
+
+// myFilter
+// const myArray = [1, 3, 5, 7, 9];
+// const arrayAnswer = myArray.filter((x) => x > 4.9999999999999999);
+// let smthNumber = 0.1+0.2;
+// console.log(smthNumber);
+// console.log(arrayAnswer);
+// myFilter
+
+// myInclude
+// const myArray = [1, 2, 3.000000000000000099999999999999999990099999999999990999999999999999999999999999999999999999999999999999999991, 4, 5];
+// const ifIncludes = myArray.includes(3.00000000000000000111);
+// console.log(ifIncludes);
+
+// const newArray = [{ id: 1 }, { id: 2 }, { id: 3 }];
+// const indexArray = newArray[2];
+// console.log(indexArray);
+// const checkIdNumber1 = myArray.includes(2, 3);
+// console.log(checkIdNumber1);
+// const checkIdNumber2 = myArray.includes(2, 1);
+// console.log(checkIdNumber2);
+
+// myInclude
